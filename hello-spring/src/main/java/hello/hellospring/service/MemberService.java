911e8956dafec1bf8a54af7af4ccf71de4738703
sawol <55649302+sawol@users.noreply.github.com>
2022-04-16 02:07:55 +0900

    memory repository를 이용한 회원 관리

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,45 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository repository;
+
+    @Autowired
+    public MemberService(MemoryMemberRepository memberRepository) {
+        this.repository = memberRepository;
+    }
+
+    public Long join(Member member){
+        validateDuplicateMember(member);    // 중복 회원 검증
+        repository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        repository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    /*
+    * 전체회원 조회
+    * */
+    public List<Member> findMembers(){
+        return repository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return repository.findById(memberId);
+    }
+
+}
